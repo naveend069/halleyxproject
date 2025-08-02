1ea0613c2c93cf2b48f543f43a0f584d73b6dbb5
naveen <emailforplacement069@gmail.com>
2025-08-02 18:29:28 +0530

    Update add-to-cart.dto.ts

--- add-to-cart.dto.ts
+++ add-to-cart.dto.ts
@@ -1,11 +1,1 @@
-// src/cart/dto/add-to-cart.dto.ts
-import { IsString, IsInt, Min } from 'class-validator';
 
-export class AddToCartDto {
-  @IsString()
-  productId: string;
-
-  @IsInt()
-  @Min(1)
-  quantity: number;
-}
